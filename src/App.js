1cf8f9b7ca165001bd4ee74f16f2d6c6aa716d27
Nimesh <nimeshjohari95@gmail.com>
2021-05-01 16:40:43 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const demoHat = () => (
+  <div>
+    <h2>Hats Header </h2>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hat" component={demoHat} />
     </div>
   );
 }
