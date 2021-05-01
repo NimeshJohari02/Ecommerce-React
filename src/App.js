de560b629205ad6aa89011ea3c04c301b08a094f
Nimesh <nimeshjohari95@gmail.com>
2021-05-01 15:58:56 +0530

    Updated Project Structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 function App() {
   return (
     <div>
@@ -7,5 +7,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
