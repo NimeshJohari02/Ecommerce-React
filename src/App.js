2f12764849f71e31ec89744d75f03db276987b40
Nimesh <nimeshjohari95@gmail.com>
2021-05-03 01:32:27 +0530

    Added Signin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header/header.component.jsx";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
