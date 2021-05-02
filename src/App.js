1b7ba2d4b4b0963fa0e20af153ed1c2c956eff25
Nimesh <nimeshjohari95@gmail.com>
2021-05-02 20:52:12 +0530

    Shop Route Made And Styled

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const demoHat = () => (
-  <div>
-    <h2>Hats Header </h2>
-  </div>
-);
+import Shop from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={demoHat} />
+        <Route exact path="/shop" component={Shop} />
       </Switch>
     </div>
   );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import SHOP_DATA from "./ShopData.js";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 class Shop extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       collections: SHOP_DATA,
     };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
