735b12985f143511428ebb4f6ebaf593625458a3
Nimesh <nimeshjohari95@gmail.com>
2021-05-07 21:15:32 +0530

    Updated Button And Added Hover Effect in Collection Item

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./button.style.scss";
-const Button = ({ children, isGoogleSignIn, ...OtherProps }) => (
+const Button = ({ children, isGoogleSignIn, inverted, ...OtherProps }) => (
   <button
-    className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    className={`${isGoogleSignIn ? "google-sign-in" : ""} ${
+      inverted ? "inverted" : ""
+    } custom-button`}
     {...OtherProps}
   >
     {children}
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../button/button.component";
 import "./collection-item.style.scss";
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
@@ -12,6 +13,9 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">${name}</span>
       <span className="price">${price}</span>
     </div>
+    <Button classname="custom-button" inverted>
+      ADD TO CART
+    </Button>
   </div>
 );
 export default CollectionItem;
