1f8d1cb92892a2cbfc206064932315949409b7ca
Nimesh <nimeshjohari95@gmail.com>
2021-05-10 23:54:44 +0530

    Added Redux Persist

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,16 @@
 import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage";
+//! This returns storage from the browser that is window.localStorage
+import { persistReducer } from "redux-persist";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-export default combineReducers({
-  //In this large json object which would be our state the key would be the slicc that points towards
-  // the reducer and the value is the value of the userReducer itself
-  user: userReducer,
-  cart: cartReducer,
-});
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  //! WhiteList allows us to add the which storage would you like to persist
+  whitelist: ["cart"],
+};
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+//In this large json object which would be our state the key would be the slicc that points towards
+// the reducer and the value is the value of the userReducer itself
+export default persistReducer(persistConfig, rootReducer);
