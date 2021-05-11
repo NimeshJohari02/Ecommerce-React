5f5bf75314a94f0df547aad3d273c0a04e78f1ea
Nimesh <nimeshjohari95@gmail.com>
2021-05-11 12:37:41 +0530

    Normlised Data

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,11 +1,4 @@
 import { createSelector } from "reselect";
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
 const SelectShop = (state) => state.shop;
 export const SelectCollections = createSelector(
   [SelectShop],
@@ -13,8 +6,7 @@ export const SelectCollections = createSelector(
 );
 
 export const SelectCollection = (collectionUrlParam) =>
-  createSelector([SelectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [SelectCollections],
+    (collections) => collections[collectionUrlParam]
   );
